0a5c9736f5b30879da89b0ba73ee2a9b5224230f
Easton Hans <hanseaston697@gmail.com>
2026-04-08 18:18:34 +0300

    Fix for duplicate files

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,14 @@ module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['**/test/**/*.test.js'],
   verbose: true,
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: '.',
+        outputName: 'junit.xml'
+      }
+    ]
+  ]
 }
